c39eaa0e61ffc09195ba2547b7544053f28d821f
Appiah <emap218@gmail.com>
2022-09-23 01:35:23 +0000

    Maps & Filters
    Scala classes and constructors

--- src/ScalaClass.scala
+++ src/ScalaClass.scala
@@ -1,6 +1,18 @@
 //Scala - Class
 
-class User(var name : String, var age : Int);
+class User(var name : String, var age : Int){
+    def printName{println(name)}
+}
+
+//val : should be considered as a constant and cannot be changed
+//has getter only
+
+//var : not a constant nor final  and can be changed
+//getter and setter are available
+
+//default : no getter , no setter
+
+//private prevents a variable from being used outside the class
 
 object Demo{
     def main(args : Array[String]): Unit ={
